7d508010bffd30963ee5ba839b3d173b192689fb
Guillermo Manzato <manzato@ekumenlabs.com>
2021-06-08 09:37:09 -0300

    Add dummy test

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,9 +4,15 @@ from tortoise_data_migration import DataMigration
 
 
 class ModelTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_name(self):
         name = "001-first-migration"
         migration = DataMigration(name=name)
+        self.assertEqual(migration.name, name)
+
+    def test_name_and_id(self):
+        name = "002-second-migration"
+        migration = DataMigration(id=4, name=name)
+        self.assertEqual(migration.id, 4)
         self.assertEqual(migration.name, name)
 
 
